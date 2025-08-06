264dafdae3afa781be553efd2793180701744cea
Edmir <User@DESKTOP-8BAG8JO>
2025-08-06 14:22:52 +0200

    connect with patient and doctor

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,35 @@
+using System.Data.Common;
+
+using System.Linq;
 using System.Diagnostics;
+using Med_Center.Data;
 using Med_Center.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Med_Center.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MedCenterContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MedCenterContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var appointments = await _context.Appointments.ToListAsync();
+            ViewBag.Appointments = appointments;
+            var dMax = appointments.GroupBy(a => a.DoctorId).OrderByDescending(g => g.Count()).FirstOrDefault()?.Key;
+            var doktori = await _context.Doctors.FindAsync(dMax);
+            ViewBag.doktoriName = doktori?.Name;
+            var pMax = appointments.GroupBy(a => a.PatientId).OrderByDescending(g => g.Count()).FirstOrDefault()?.Key;
+            var pacienti = await _context.Patients.FindAsync(pMax);
+            ViewBag.pacientName = pacienti?.FirstName + " " + pacienti?.LastName;
             return View();
         }
 
